68c7d02b5e952d91c9dcd941eb84b2f204a96c17
Kian Ganz <kian.ganz@gmx.ch>
2022-09-15 14:46:57 +0200

    add gitignore

--- time/Time.ts
+++ time/Time.ts
@@ -1,9 +1,10 @@
-var today = new Date();
+const today = new Date();
 var userInputDate = new Date(2022,10,12,18,12);
 
-var diff = Math.abs(today.getTime() - userInputDate.getTime());
+var diff = Math.ceil(today.getTime() - userInputDate.getTime());
 var diffDays = Math.ceil(diff / (1000 * 3600 * 24)); 
 var duration = userInputDate.valueOf() - today.valueOf();
 
 console.log(diff)
 console.log(duration)
+console.log(diffDays)
